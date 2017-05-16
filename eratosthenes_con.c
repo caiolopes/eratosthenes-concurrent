91c6defd0f9c9d95c7665eedbd97a141f2dd5f7e
Guilherme Andriotti Momesso <gmomesso@localhost.localdomain>
2017-05-16 16:01:37 -0300

    eratosthenes_con now is equivalent to eratosthenes_seq but running 4 threads.

--- eratosthenes_con.c
+++ eratosthenes_con.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <pthread.h>
+//#include <time.h>
 
+// Usage: time ./e_con n t
 int n;
 int n_max;
 unsigned char* primes;
@@ -21,8 +23,10 @@ void run() {
         i = n;
         n++;
         pthread_mutex_unlock(&index_cond_lock);
-        for (int j = 2; i*j <= n_max; j++) {
-            primes[i*j] = 0;
+        if (primes[i] == 1) {
+            for (int j = 2; i*j <= n_max; j++) {
+                primes[i*j] = 0;
+            }
         }
     }
     return;
@@ -34,7 +38,7 @@ int main (int argc, char **argv) {
     if (argc > 1) {
         n_max = atoi(argv[1]);
         int n_threads = atoi(argv[2]);
-        primes = (unsigned char*)malloc(sizeof(unsigned char) * n_max);
+        primes = (unsigned char*)malloc(sizeof(unsigned char) * (n_max+1));
         for (int i = 0; i <= n_max; i++) {
           primes[i] = 1;
         }
@@ -44,6 +48,7 @@ int main (int argc, char **argv) {
         pthread_mutex_init(&index_cond_lock, NULL);
 
         n = 2;
+        //clock_t begin = clock();
         for(int i = 0; i < n_threads; i++) {
             if (pthread_create(&threads[i], 0, (void *) run, (void *) 0) != 0) {
                 printf("ERROR - pthread_created failed!\n");
@@ -54,10 +59,22 @@ int main (int argc, char **argv) {
         for(int i = 0; i < n_threads; i++) {
             pthread_join(threads[i], 0);
         }
+        //clock_t end = clock();
+
+        //double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+        //printf("time spent: %f s\n", time_spent);
     } else {
         printf("usage: ./e_con n t [--print]\n"
                "n: maximum number\n"
                "t: number of threads\n");
+    }
+
+    if (argc > 3) {
+      for (int i = 0; i <= n_max; i++) {
+        if (primes[i] == 1) {
+          printf("%d\n", i);
+        }
+      }
     }
 
     return 0;
